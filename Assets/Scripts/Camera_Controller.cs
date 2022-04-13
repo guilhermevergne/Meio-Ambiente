35776c59c504739d7bb6c83c17a583cdd180319e
guilhermevergne <guilhermevergne@gmail.com>
2022-04-12 23:52:22 -0300

    Novo Refeitório
    Refeitório atualizado
    Personagem selecionado no refeitório

--- Assets/Scripts/Camera_Controller.cs
+++ Assets/Scripts/Camera_Controller.cs
@@ -7,9 +7,26 @@ public class Camera_Controller : MonoBehaviour
     public GameObject player;
     Vector3 velocity = Vector3.zero;
     float smoothness = 0.25f;
+    int n = 0;
+    bool done = false;
+
 
     private void FixedUpdate()
     {
+        if (n > 10 && !done)
+        {
+            string charTag = PlayerPrefs.GetString("gender");
+            charTag += PlayerPrefs.GetString("color");
+            charTag += PlayerPrefs.GetString("hair");
+            player = GameObject.Find(charTag+"(Clone)");
+            print(charTag);
+            done = true;
+            n = -1;
+        }
+        else if (n >= 0)
+        {
+            n++;
+        }
         transform.position = Vector3.SmoothDamp(transform.position, new Vector3(
                 player.transform.position.x, transform.position.y, transform.position.z),
                 ref velocity, smoothness);
