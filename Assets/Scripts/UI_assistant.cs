eda1db7667675ff4412315238b9565c01bc98437
Marcelo H C D Filho <49722276+GoblinLayer@users.noreply.github.com>
2022-06-08 17:57:42 -0300

    Text Fix
    texto falta so os botoes

--- Assets/Scripts/UI_assistant.cs
+++ Assets/Scripts/UI_assistant.cs
@@ -18,7 +18,7 @@ public class UI_assistant : MonoBehaviour
 
     private void Awake()
     {
-        
+
 
         if (SceneManager.GetActiveScene().buildIndex == 0)
         {
@@ -33,7 +33,6 @@ public class UI_assistant : MonoBehaviour
                 else
                 {/*
                     string[] messageArray = new string[]{
-
                 };
                     string message = messageArray[i];
                     textWriterSingle = TextWriter.AddWriter_Static(messageText, message, .05f, true, true);*/
@@ -90,7 +89,7 @@ public class UI_assistant : MonoBehaviour
         else if (SceneManager.GetActiveScene().buildIndex == 2)
         {
             messageText = transform.Find("DialogueText").GetComponent<Text>();
-            if(PlayerPrefs.GetInt("Jogo da Pia") == 0)
+            if (PlayerPrefs.GetInt("Jogo da Pia") == 0)
             {
                 textWriterSingle = TextWriter.AddWriter_Static(messageText, "Este é o refeitório, aqui você fará uma refeição. " +
                     "Mas antes de se servir, primeiro lave as suas mãos.", .02f, true, true);
@@ -125,53 +124,27 @@ public class UI_assistant : MonoBehaviour
                 i++;
             };
         }
-        else if(SceneManager.GetActiveScene().buildIndex == 3)
+        else if (SceneManager.GetActiveScene().buildIndex == 3)
         {
             messageText = transform.Find("DialogueText").GetComponent<Text>();
 
             //display = GameObject.FindGameObjectWithTag("TextDisplay");
-            //display.SetActive(display.activeSelf);
+            //display.SetActive(display.activeSelf);messageText = transform.Find("DialogueText").GetComponent<Text>();
 
-            textWriterSingle = TextWriter.AddWriter_Static(messageText, "Agora você deve escolher a torneira mais adequada para lavar suas mãos.", .02f, true, true);
-
-
-            if (textWriterSingle != null && textWriterSingle.isactive())
+            transform.Find("DialogueText").GetComponent<Button_UI>().ClickFunc = () =>
             {
-                textWriterSingle.WriteAllandDestroy();
-            }
-            else
-            {
-                string[] messageArray = new string[]{
-                    "Clique no computador para realizar o seu cadastro!",
-                    "Oh oh, infelizmente você não pode adentrar o SENAI Cimatec com as vestimentas escolhidas. Por favor, volte e escolha novamente.",
-                    "Parabéns, você escolheu vestimentas adequadas para adentrar o SENAI Cimatec. Sua entrada está liberada !!!!",
-                    "Mas atenção, você está recebendo 1.000 sustens para serem utilizadas nas próximas missões. \nPortanto, cuidado com as suas escolhas, pois elas impactam na sua pontuação final!!!!",
-                };
-                if (i == 0)
+
+                if (textWriterSingle != null && textWriterSingle.isactive())
                 {
-                    string message = messageArray[i];
-                    textWriterSingle = TextWriter.AddWriter_Static(messageText, message, .05f, true, true);
-                    i = 1;
+                    textWriterSingle.WriteAllandDestroy();
                 }
-                else if (i == 1 && PlayerPrefs.GetString("charGen") == "Vestimenta inadequada!")
+                else
                 {
-                    string message = messageArray[i];
-                    textWriterSingle = TextWriter.AddWriter_Static(messageText, message, .05f, true, true);
-                }
-                else if (i == 1 && PlayerPrefs.GetString("charGen") == "Created")
-                {
-                    string message = messageArray[i + 1];
-                    textWriterSingle = TextWriter.AddWriter_Static(messageText, message, .05f, true, true);
-                    i = 2;
-                }
-                else if (i == 2)
-                {
-                    string message = messageArray[i + 1];
-                    textWriterSingle = TextWriter.AddWriter_Static(messageText, message, .05f, true, true);
-                    i = 3;
+                   
+
                 }
 
-            }
+            };
         }
 
 
@@ -186,7 +159,7 @@ public class UI_assistant : MonoBehaviour
             "conseguir a maior recompensa de todas, receber a coroa da sustentabilidade!", .02f, true, true);
 
         }
-        else if(SceneManager.GetActiveScene().buildIndex == 1)
+        if (SceneManager.GetActiveScene().buildIndex == 1)
         {
             if (charcreate)
             {
@@ -198,7 +171,11 @@ public class UI_assistant : MonoBehaviour
                 "sua aventura aqui, primeiro é necessario que se cadastre no sistema para ter seu acesso liberado.", .02f, true, true);
             }
         }
-        
+        if (SceneManager.GetActiveScene().buildIndex == 3)
+        {
+            textWriterSingle = TextWriter.AddWriter_Static(messageText, "Escolha a pia mais ecologicamente adequada!", .05f, true, true);
+        }
+
 
     }
 }
