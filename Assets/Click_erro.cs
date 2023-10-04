2557c7edf162969703316ae621a293317175c9bd
guilhermevergne <guilhermevergne@gmail.com>
2023-10-04 16:22:55 -0300

    Click event no sprite

---
+++ Assets/Click_erro.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click_erro : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Debug.Log("Sprite Clicked");
+    }
+}
